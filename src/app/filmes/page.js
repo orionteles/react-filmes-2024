8ff24a32c9cdca816be4513e24c570c7d71b9969
Orion Teles de Mesquita <orionteles@gmail.com>
2024-09-05 21:57:44 -0300

    Projeto de Filmes 2024

---
+++ src/app/filmes/page.js
@@ -0,0 +1,41 @@
+'use client'
+
+import Pagina from "@/app/components/Pagina";
+import { useEffect, useState } from "react";
+import apiMovie from "@/app/services/apiMovies";
+import { Card, Col, Row } from "react-bootstrap";
+
+export default function Page() {
+
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(() => {
+        apiMovie.get('movie/popular').then(resultado => {
+            setFilmes(resultado.data.results)
+        })
+    }, [])
+
+    return (
+        <Pagina titulo="Filmes">
+
+            <Row md={3}>
+                {filmes.map(item => (
+                    <Col key={item.id} className="mt-3">
+                        <Card>
+                            <Card.Img height={150} variant="top" src={'https://image.tmdb.org/t/p/w500/' + item.backdrop_path} />
+                            <Card.Body>
+                                <Card.Title>{item.title}</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+
+            {filmes.map(item => (
+                <p>{item.title}</p>
+            ))}
+
+        </Pagina>
+    )
+}
+
